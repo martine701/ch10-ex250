d585f89b10ff3854e1c4042bd8e9374a9235fefa
Martine Alliet <allietmartine@gmail.com>
2023-07-20 01:22:25 +0100

    commit wed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,14 @@
 import { Component } from '@angular/core';
+import { Car } from './car';
 
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
     <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
+      <li *ngFor = "let car of _cars">
+        <span [attr.id]="car.id" [attr.data.desc]="car.make+''+car.model" [attr.data-article]="car.article">
+          {{car.year}}&nbsp;{{car.make}}&nbsp;{{car.model}}&nbsp;
+          <button (click)="showCar($event)">View</button></span>
       </li>
     </ul>
     
@@ -28,5 +16,20 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class AppComponent {
-  title = 'ch10-ex250';
+  _cars = [
+    new Car('car1', 2002, 'bmw', 'm3', 'https://en.wikipedia.org/wiki/BMW_M3'),
+    new Car('car2', 2017, 'acura', 'nsx', 'https://en.wikipedia.org/wiki/Honda_NSX'),
+    new Car('car3', 2016, 'chevy', 'camaro', 'https://en.wikipedia.org/wiki/Chevrolet_Camaro')
+  ];
+
+  showCar(event:any) {
+    const desc = event.target.parentElement.dataset.descl
+    if (window.confirm('If you click "ok" you would be redirected to an article about the' +
+      desc + '.Cancel will load this website'))
+    {
+      window.location.href = event.target.parentElement.dataset.article;
+    };
+
+  }
+
 }
---
+++ src/app/car/car.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car',
+  template: `
+    <p>
+      car works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CarComponent {
+
+}
